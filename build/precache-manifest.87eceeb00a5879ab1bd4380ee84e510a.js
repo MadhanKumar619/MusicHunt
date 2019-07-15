7a85eb714a94c90411c1d1233fc8b61896ef5867
Madhansmart <madhansmart7404@gmail.com>
2019-07-15 23:25:57 +0530

    First Commit

---
+++ build/precache-manifest.87eceeb00a5879ab1bd4380ee84e510a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e327c775bab1a14044e88c05524de7",
+    "url": "/MusicHuntR/index.html"
+  },
+  {
+    "revision": "ff13f03e343be875803b",
+    "url": "/MusicHuntR/static/css/2.427492a1.chunk.css"
+  },
+  {
+    "revision": "ac5f1f5a5ff64864ee2f",
+    "url": "/MusicHuntR/static/css/main.7bf30941.chunk.css"
+  },
+  {
+    "revision": "ff13f03e343be875803b",
+    "url": "/MusicHuntR/static/js/2.28c1ecf3.chunk.js"
+  },
+  {
+    "revision": "ac5f1f5a5ff64864ee2f",
+    "url": "/MusicHuntR/static/js/main.6ffee267.chunk.js"
+  },
+  {
+    "revision": "907515147ae2a49eda8b",
+    "url": "/MusicHuntR/static/js/runtime~main.f5748913.js"
+  },
+  {
+    "revision": "806430aaaee91d74f89d8fd547cb2fed",
+    "url": "/MusicHuntR/static/media/logo.806430aa.png"
+  }
+]);
